f0cdda1c7c6b6d07f4cff2916c8ba4b38a66f0cc
DESKTOP-KKHKH84\Rio.Rai <hixbunny02@gmail.com>
2018-05-08 19:40:29 +0700

    contructor getter setter of taikhoan

---
+++ WebThiTuLuan/src/java/model/TaiKhoan.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Rio.Rai
+ */
+public class TaiKhoan {
+    
+    private String userName;
+    private String passWord;
+
+    public TaiKhoan() {
+    }
+
+    public TaiKhoan(String userName, String passWord) {
+        this.userName = userName;
+        this.passWord = passWord;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+    
+    
+}
